fad3350b702e078675caba6d73f3b1a5e47b451f
omerSut <omer.sut@santsg.com>
2023-05-09 09:35:20 +0300

    added new function for open github repository in vscode web

--- popup.js
+++ popup.js
@@ -11,8 +11,23 @@ function logTestMessage() {
     });
 }
 
+
+function showInVsCode() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let tab = tabs[0];
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: () => {
+                window.location.href = window.location.href.replace("github", "vscode.dev/github");
+                
+            }
+        });
+    });
+}
+
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     let tab = tabs[0];
     document.getElementById("log-button").addEventListener("click", logTestMessage);
+    document.getElementById("github-button").addEventListener("click", showInVsCode);
 });
 
